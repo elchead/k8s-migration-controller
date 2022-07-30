79a7876adc19e6f41c7445789712599dce64a13f
Adrian Stobbe <adrian.stobbe@sap.com>
2022-07-30 11:19:57 +0200

    fix

--- pkg/migration/main.go
+++ pkg/migration/main.go
@@ -1,8 +1,6 @@
 package migration
 
-import "github.com/coredns/corefile-migration/migration"
-
 func main() {
 	pod := "o10n-worker-m-hgvh5-hjjbm"
-	migration.Migrate([]MigrationCmd{{Pod:pod,NewNode:"zone2",}})
+	Migrate([]MigrationCmd{{Pod:pod,NewNode:"zone2"}},"playground")
 }
